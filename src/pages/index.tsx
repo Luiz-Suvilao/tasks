feb68f418b872f36b64106e61a15d4b7ab09005e
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-07 20:11:47 -0300

    creating a home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/pages/index.module.scss';
 
 export default function Home() {
     return (
@@ -7,9 +10,25 @@ export default function Home() {
                 <title>Tarefas | Home</title>
             </Head>
 
-            <div>
-                <h1>Olá</h1>
-            </div>
+            <main className={styles.contentContainer}>
+                <img src='/images/board-user.svg' alt='Ícone de um notebook com o site aberto.' />
+
+                <section className={styles.CTA}>
+                    <h1>Uma ferramenta para seu dia a dia. Escreva, planeje e organize-se...</h1>
+                    <p>
+                        <span>100% gratuita</span> e online.
+                    </p>
+                </section>
+
+                <h1 className={styles.donorsTitle}>Doadores</h1>
+
+                <div className={styles.donors}>
+                    <img
+                        src="https://sujeitoprogramador.com/steve.png"
+                        alt="Usuário"
+                    />
+                </div>
+            </main>
         </>
     );
 }
