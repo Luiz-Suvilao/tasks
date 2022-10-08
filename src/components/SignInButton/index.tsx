5a0345196ed840f401d18bb1be628cf4aa7f468a
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-07 21:42:17 -0300

    using real data from session

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,25 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './sigInButton.module.scss';
 
 export function SignInButton() {
-    const session = true;
+    const { data: session } = useSession();
 
     return session ? (
         <button
             type="button"
             className={styles.signInButton}
-            onClick={() => {
-            }}
+            onClick={() => signOut()}
         >
             <img
-                src="https://sujeitoprogramador.com/steve.png"
-                alt="Usuário"
+                src={session.user.image}
+                alt={`Foto de usuário do ${session.user.name}`}
             />
 
-            Olá Filipe
+            Olá {(session.user.name || 'Nome não localizado')}
             <FiX
                 color="#737380"
                 className={styles.closeIcon}
@@ -28,10 +29,9 @@ export function SignInButton() {
         <button
             type="button"
             className={styles.signInButton}
-            onClick={() => {
-            }}
+            onClick={() => signIn('github')}
         >
-            <FaGithub color="#ffb800" />
+            <FaGithub color="#ffb800"/>
             Entrar com GitHub
         </button>
     );
