e56afc2184006a3d1841e9bd08930b121b583d91
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-07 22:01:59 -0300

    configure callbacks on login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,10 +8,34 @@ export const authOptions = {
             clientSecret: process.env.GITHUB_SECRET,
             authorization: {
                 params: {
-                    scope: 'read:user'
+                    scope: 'read:user user:email'
                 }
             }
         }),
     ],
+    callbacks: {
+        async session({ session, user, token }) {
+            try {
+                return {
+                    ...session,
+                    id: user.id
+                };
+            } catch {
+                return {
+                    ...session,
+                     id: null,
+                };
+            }
+        },
+
+        async signIn({ user, account, profile, email, credentials }) {
+            try {
+                // console.log(user); id, name, email, img
+                return true;
+            } catch (err) {
+                return false;
+            }
+        },
+    }
 }
 export default NextAuth(authOptions)
