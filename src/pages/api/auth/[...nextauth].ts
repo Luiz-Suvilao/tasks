788d85c3ebbef22a8d1b39efd4b93f1fbf00a294
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-07 21:25:38 -0300

    configure scopes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,11 @@ export const authOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+            authorization: {
+                params: {
+                    scope: 'read:user'
+                }
+            }
         }),
     ],
 }
