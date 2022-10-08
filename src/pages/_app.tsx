ba231ee48a0189e8eb3dea251a9aae207e81e45a
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-07 21:26:05 -0300

    using SessionProvider for getting session data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,26 @@
 import { AppProps } from 'next/app';
+import { Session } from 'next-auth';
+
+import { SessionProvider } from 'next-auth/react';
 
 import { Header } from '../components/Header';
 import { DonationButton } from '../components/DonationButton';
 
 import '../styles/global.scss';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({
+    Component,
+    pageProps,
+}: AppProps<{
+    session: Session;
+}>) {
     return (
-        <>
-            <Header />
+        <SessionProvider session={pageProps.session}>
+            <Header/>
             <Component {...pageProps} />
-            <DonationButton />
-        </>
-    )
+            <DonationButton/>
+        </SessionProvider>
+    );
 }
 
 export default MyApp
