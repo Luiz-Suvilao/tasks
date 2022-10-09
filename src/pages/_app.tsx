93cc24ae270e616e77cc5bb6b043966edb3b30bd
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-09 00:56:07 -0300

    add paypal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import { Session } from 'next-auth';
-
 import { SessionProvider } from 'next-auth/react';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 import { Header } from '../components/Header';
 import { DonationButton } from '../components/DonationButton';
@@ -14,11 +14,19 @@ function MyApp({
 }: AppProps<{
     session: Session;
 }>) {
+    const initialPayPalOptions = {
+        'client-id': 'ASomlkeNijYR9XsBLXJc0P3t60ohOy1PofKRrNWjG3oio4HO_xdlEZgPpibvcOobSQt7c18L4vZoTqOM',
+        currency: 'BRL',
+        intent: 'capture'
+    };
+
     return (
         <SessionProvider session={pageProps.session}>
-            <Header/>
-            <Component {...pageProps} />
-            <DonationButton/>
+            <PayPalScriptProvider options={initialPayPalOptions}>
+                <Header/>
+                <Component {...pageProps} />
+                <DonationButton/>
+            </PayPalScriptProvider>
         </SessionProvider>
     );
 }
