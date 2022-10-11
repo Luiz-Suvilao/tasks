0221ceba4009873d54ab5d22300bd6b5cd0f745f
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-11 13:23:54 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { BsFillBookmarkCheckFill } from 'react-icons/bs'
 
 export default class MyDocument extends Document {
     render(): JSX.Element {
         return (
             <Html>
                 <Head>
+                    <link rel="icon" type="image/svg" href="task-list.svg" />
                     <meta name="description" content="Crie e gerencie suas tarefas com a melhor ferramenta da web" />
                 </Head>
 
