96f13add943871f314e5db428345beae8a5dc0b7
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-11 16:20:10 -0300

    enabling cors

--- next.config.js
+++ next.config.js
@@ -3,9 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com']
   },
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx'],
+  async headers() {
+    return [
+      {
+        source: "/api/email/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
