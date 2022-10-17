ccefdafbb7a26c01362f94f1de525e6fd616c4b7
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-10-17 19:26:38 -0300

    adjust text email message

--- src/pages/api/email/successLogin.tsx
+++ src/pages/api/email/successLogin.tsx
@@ -18,7 +18,7 @@ export default async function (req, res) {
     const html = `
         <div>
             <h3>Login efetuado com sucesso! Aproveite nossa plataforma :)</h3>
-            <p>Qualquer dúvida ou sugestão, responda esse endereço eletrônico. Teremos o maior prazer em ouvi-lo(a)</p>
+            <p>Qualquer dúvida ou sugestão, entre em contato com as redes abaixo. Teremos maior prazer em ouvi-lo(a)</p>
             
             <h4>Fale comigo através das seguintes redes</h4>
             <a href="https://www.instagram.com/luiz_filipe.dev/">Instagram</a> ou <a href="https://www.linkedin.com/in/luiz-filipe-490a02182/">Linkedin</a>        
